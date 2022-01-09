da0a08a38b9b44d3c8ea05da14ba00eed89f5fb4
study2340992 <study2340992@gmail.com>
2022-01-09 15:32:24 +0530

    First release of Hello World!

--- src/main/java/com/bizruntime/ids/controller/BookRestController.java
+++ src/main/java/com/bizruntime/ids/controller/BookRestController.java
@@ -1,18 +1,29 @@
 package com.bizruntime.ids.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bizruntime.ids.service.BookRestService;
 
 @RestController
 @RequestMapping(value = "/hello", produces = "text/plain")
 @CrossOrigin
 public class BookRestController {
 
+	@Autowired
+	private BookRestService bookRestService;
+
+	public BookRestController() {
+
+	}
+
 	@GetMapping("/{id}")
 	public String getMessage() {
-		return "Hello World!!";
+		return bookRestService.getMessage();
 	}
 
 }
---
+++ src/main/java/com/bizruntime/ids/service/BookRestService.java
@@ -0,0 +1,11 @@
+package com.bizruntime.ids.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookRestService {
+
+	public String getMessage() {
+		return "Hello World!!!";
+	}
+}
